c1d91e23c9ac9d503d0aed5dc3ad1c1389c6676d
david63011 <david630@gmail.com>
2023-02-24 19:01:20 -0300

    add

--- db/migrate/20230224175709_create_create_comments.rb
+++ db/migrate/20230224175709_create_create_comments.rb
@@ -1,8 +1,7 @@
-# frozen_string_literal: true
-
+#create_table 
 class CreateCreateComments < ActiveRecord::Migration[7.0]
   def change
-    create_table :comments do |t|
+    create_table :create_comments do |t|
       t.text :text
       t.timestamps
     end
