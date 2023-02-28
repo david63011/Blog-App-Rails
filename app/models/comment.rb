f28fac2a3736e1c15726194b9460cd3b4377b314
david63011 <david630@gmail.com>
2023-02-28 16:41:20 -0300

    add required changes

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
-    belongs_to :posts, class_name: 'Post', foreign_key: :create_posts_id
+  belongs_to :posts, class_name: 'Post', foreign_key: :create_posts_id
   has_many :comments, foreign_key: :create_posts_id
 
   def update_comments_counter
