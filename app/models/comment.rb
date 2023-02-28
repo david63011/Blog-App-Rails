37fb51ad8db8de8f58a7c53372a69997d2e6ea6d
david63011 <david630@gmail.com>
2023-02-28 16:47:39 -0300

    lintors

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,4 @@
+# Model for comments
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   belongs_to :posts, class_name: 'Post', foreign_key: :create_posts_id
