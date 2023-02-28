9ea6974470a66a640892966d0ad4f3bcbc2b9698
david63011 <david630@gmail.com>
2023-02-28 10:14:22 -0300

    lintors

--- app/models/create_like.rb
+++ app/models/create_like.rb
@@ -3,8 +3,7 @@ class CreateLike < ApplicationRecord
   belongs_to :create_posts, class_name: 'CreatePost', foreign_key: :create_posts_id
   after_save :update_likes_counter
 
-
-  def update_likes_counter
+ def update_likes_counter
     post.increment(:likes_counter)
   end
 end
