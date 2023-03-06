99ff7a17e29ae2a4e1e4b799e195a81f3a1b2058
david63011 <david630@gmail.com>
2023-03-06 14:48:53 -0300

    add control

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :users, only: [:index,:show] do
+  resources :posts, only: [:index,:show] 
 end
+root "users#index"
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
+
+
